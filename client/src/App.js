4dc0cc5c5c91a377e68499e956b41c94e7cfed39
Garry Brother <garry.m.brother@gmail.com>
2022-12-26 11:31:29 -0500

    Updated title

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator
+            Garry's Fibonacci Calculator for Kubernetes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
